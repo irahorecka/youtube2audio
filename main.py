478f0ae4d08f32a3a96ed7753ef687d32bdc7b37
Ira Horecka <ira89@icloud.com>
2020-04-28 00:14:09 -0700

    refactor main.py and song_property.py

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ def seconds_to_mmss(seconds):
     min = seconds // 60
     sec = seconds % 60
     if min < 10:
-        min_str = '0' + str(min)
+        min_str = "0" + str(min)
     else:
         min_str = str(min)
     if sec < 10:
-        sec_str = '0' + str(sec)
+        sec_str = "0" + str(sec)
     else:
         sec_str = str(sec)
     return min_str + ":" + sec_str
@@ -33,6 +33,7 @@ def seconds_to_mmss(seconds):
 
 class UrlLoading(QThread):
     """ Loads the videos data from playlist in another thread."""
+
     countChanged = pyqtSignal(dict, bool)
 
     def __init__(self, playlist_link, parent=None):
@@ -41,19 +42,23 @@ class UrlLoading(QThread):
 
     def run(self):
         """ Main function, gets all the playlist videos data, emits the info dict"""
-        ydl_opts = {'ignoreerrors': True, 'quiet': True}
+        ydl_opts = {"ignoreerrors": True, "quiet": True}
         videos_dict = dict()
         try:
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                playlist_dict = ydl.extract_info(self.playlist_link, download=False)  # time consuming spot -- cannot thread
-                for video in playlist_dict['entries']:
+                playlist_dict = ydl.extract_info(
+                    self.playlist_link, download=False
+                )  # time consuming spot -- cannot thread
+                for video in playlist_dict["entries"]:
                     try:
                         title = video.get("title")
                     except:  # If video title is unavailable don't add it to the dict
                         continue
                     videos_dict[title] = dict()
                     videos_dict[title]["id"] = video.get("id")
-                    videos_dict[title]["duration"] = seconds_to_mmss(video.get("duration"))
+                    videos_dict[title]["duration"] = seconds_to_mmss(
+                        video.get("duration")
+                    )
                 self.countChanged.emit(videos_dict, True)
         except:
             self.countChanged.emit({}, False)
@@ -74,21 +79,26 @@ class MainPage(QMainWindow, UiMainWindow):
         self.download_button.clicked.connect(self.download_button_click)
         self.download_path.clicked.connect(self.get_file_dir)
         # Get the desktop path, set folder name, full download path, set label.
-        self.download_dir = os.path.dirname(os.path.abspath(__file__))  # base no-selection download dir
-        self.download_folder_select.setText("Folder: ../{}".format([i for i in self.download_dir.split('/')][-1]))
+        self.download_dir = os.path.dirname(
+            os.path.abspath(__file__)
+        )  # base no-selection download dir
+        self.download_folder_select.setText(
+            "Folder: ../{}".format([i for i in self.download_dir.split("/")][-1])
+        )
 
-
-# Input url threading
+    # Input url threading
 
     def url_loading_button_click(self):
         """ Reads input data from url_input and creates an instance of the UrlLoading thread """
         self.listWidget.clear()  # Clear the widget
         self.videos_dict = dict()  # Clear the dict
         self.url_fetching_data_label.show()  # Show the loading label
-        self.url_error_label.hide()          # Hide the error label if the input is a retry
+        self.url_error_label.hide()  # Hide the error label if the input is a retry
         playlist_url = self.url_input.text()  # Get the input text
         self.calc = UrlLoading(playlist_url)  # Pass in the input text
-        self.calc.countChanged.connect(self.url_loading_finished)  # connect with the changing variables
+        self.calc.countChanged.connect(
+            self.url_loading_finished
+        )  # connect with the changing variables
         self.calc.start()
 
     def url_loading_finished(self, videos_dict, executed):
@@ -98,7 +108,9 @@ class MainPage(QMainWindow, UiMainWindow):
             videos_list, counter = list(), 0
             for key, value in videos_dict.items():
                 counter += 1
-                line_str = str(counter) + ") " + key + "  " + videos_dict[key]["duration"]  # Display line
+                line_str = (
+                    str(counter) + ") " + key + "  " + videos_dict[key]["duration"]
+                )  # Display line
                 videos_list.append(line_str)
             self.videos_dict = videos_dict
             self.listWidget.addItems(videos_list)  # Update the list with the strings
@@ -106,27 +118,35 @@ class MainPage(QMainWindow, UiMainWindow):
             self.url_error_label.show()  # Show the error label
 
     def get_file_dir(self):
-        self.download_dir = QFileDialog.getExistingDirectory(self, 'Open folder', os.path.dirname(os.path.abspath(__file__)))
+        self.download_dir = QFileDialog.getExistingDirectory(
+            self, "Open folder", os.path.dirname(os.path.abspath(__file__))
+        )
         # set nearby names
-        self.download_folder_select.setText("Folder: ../{}".format('/'.join([i for i in self.download_dir.split('/')][-2:])))
+        self.download_folder_select.setText(
+            "Folder: ../{}".format(
+                "/".join([i for i in self.download_dir.split("/")][-2:])
+            )
+        )
 
-
-# Downloading videos threading
+    # Downloading videos threading
 
     def download_button_click(self):
         """ Executes when the button is clicked """
         self.download_button.setEnabled(False)
         self.downloaded_label.setText("Downloading...")
-        self.down = DownloadingVideos(self.videos_dict, self.download_dir, self.turbo_enable)  # Pass in the dict
+        self.down = DownloadingVideos(
+            self.videos_dict, self.download_dir, self.turbo_enable
+        )  # Pass in the dict
         self.down.start()
         # TODO: Fix this below -- be able to reflect emission
         self.downloaded_label = self.down.downloadCount
 
-
     def remove_selected_items(self):
         """ Removes the selected items from the self.videos_dict and self.list_of_titles
         Also refreshes the listWidget"""
-        list_items = self.listWidget.selectedItems()  # list of the selected items (should not be a list but okay)
+        list_items = (
+            self.listWidget.selectedItems()
+        )  # list of the selected items (should not be a list but okay)
         selected_item_index = self.listWidget.currentRow()  # index of the selected item
         if not list_items:  # if nothing is selected
             return
@@ -134,16 +154,19 @@ class MainPage(QMainWindow, UiMainWindow):
             self.listWidget.takeItem(self.listWidget.row(item))
 
         if selected_item_index != -1:  # -1 means nothing was selected
-            index, delete_key = 0, ''
+            index, delete_key = 0, ""
             for key, value in self.videos_dict.items():
                 if index == selected_item_index:
-                    delete_key = key  # save the key of the deleted item in the videos_dict
+                    delete_key = (
+                        key  # save the key of the deleted item in the videos_dict
+                    )
                 index += 1
             del self.videos_dict[delete_key]  # delete the actual key
 
 
 class DownloadingVideos(QThread):
     """ Download all videos from the videos_dict using the id, todo fix some bugs"""
+
     downloadCount = pyqtSignal(str)  # downloaded, number_of_videos, finished
 
     def __init__(self, videos_dict, download_path, turbo_bool, parent=None):
@@ -159,17 +182,18 @@ class DownloadingVideos(QThread):
         # Download
         number_of_videos = len(self.videos_dict)
         failed_download = list()
-        
         time0 = time.time()
 
-        # run multithread?
-        if self.turbo_bool.isChecked(): # enable futures
-            concurrent_args = ((
-                (key, value),
-                self.yt_link_starter,
-                self.download_path) for key, value in self.videos_dict.items())
-            streams = _futures_threads(thread_download, concurrent_args)
-            shutil.rmtree(os.path.join(self.download_path, "mp4"))  # remove mp4 from dir
+        if self.turbo_bool.isChecked():  # enable futures
+            video_properties = (
+                ((key, value), self.yt_link_starter, self.download_path)
+                for key, value in self.videos_dict.items()
+            )
+            streams = download_threads(video_download, video_properties)
+            shutil.rmtree(
+                os.path.join(self.download_path, "mp4")
+            )  # remove mp4 from dir
+
             time1 = time.time()
 
         else:
@@ -177,7 +201,9 @@ class DownloadingVideos(QThread):
                 full_link = self.yt_link_starter + self.videos_dict[key]["id"]
                 try:
                     video = YouTube(full_link)
-                    stream = video.streams.filter(only_audio=True, audio_codec="mp4a.40.2").first()
+                    stream = video.streams.filter(
+                        only_audio=True, audio_codec="mp4a.40.2"
+                    ).first()
                     stream.download(self.download_path)
                 except:
                     failed_download.append(key)
@@ -190,77 +216,71 @@ class DownloadingVideos(QThread):
         self.downloadCount.emit(f"Download time: {'%.2f' % delta_t} seconds")
 
 
-def _futures_threads(transform, iterable):
-    import time
-    with concurrent.futures.ThreadPoolExecutor() as executor:  # a bunch of executors in this futures class
-        streams = executor.map(transform, iterable, timeout=1)  # again, a functional programming paradigm using map method
+def download_threads(transform, iterable):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        streams = executor.map(
+            transform, iterable, timeout=1
+        )
     return streams
 
 
-def thread_download(args):
+def video_download(args):
     # NOTE: this must have no relation to any self obj
     key_value, videos_dict = args[0]
     yt_link_starter = args[1]
     download_path = args[2]
-    full_link = yt_link_starter + videos_dict['id']
+    full_link = yt_link_starter + videos_dict["id"]
 
     try:
+        # TODO: could be strange to create new folder/path in multithread - no collision yet but maybe a source of a bug
+        mp4_path = os.path.join(download_path, "mp4")
         video = YouTube(full_link)
         stream = video.streams.filter(only_audio=True, audio_codec="mp4a.40.2").first()
-        # could be strange to create new folder/path in multithread - no collision yet but maybe a source of a bug
-        mp4_dir = os.path.join(download_path, "mp4")
-        stream.download(mp4_dir)
+        stream.download(mp4_path)
+
         # convert generated mp4 files to mp3 -- problem with MacOS where mp4 is twice length of video
         mp4_filename = stream.default_filename  # mp4 full extension
         mp3_filename = "{}.mp3".format(mp4_filename[:-4])
         filename = mp4_filename[:-4]
 
-        subprocess.call([
-            'ffmpeg',
-            '-i', os.path.join(mp4_dir, mp4_filename),
-            os.path.join(download_path, mp3_filename)
-        ])
-        thumbnail_download(download_path, full_link, filename)
+        subprocess.call(
+            [
+                "ffmpeg",
+                "-i",
+                os.path.join(mp4_path, mp4_filename),
+                os.path.join(download_path, mp3_filename),
+            ]
+        )
+        
+        album_artwork_download(download_path, full_link, filename)
 
-        return
+        return        
     except:
         pass
 
-def thumbnail_download(directory, vid_url, vid_name):
-    """Download video thumbnail and add to mp3 file"""
-    file_jpg = f"{vid_name}.jpg"
+
+def album_artwork_download(directory, vid_url, vid_name):
+    """Download album artwork and add to mp3 file"""
     file_mp3 = f"{vid_name}.mp3"
+    ITUNES_META_JSON = get_itunes_metadata(vid_url)
+    print(ITUNES_META_JSON)
 
-    # download thumbnail
-    # youtube_id = vid_url.partition("?v=")[2]
-    # thumb_nail_download = f"https://img.youtube.com/vi/{youtube_id}/maxresdefault.jpg"
-    # x = requests.get(thumb_nail_download, stream=True)
-    # with open(os.path.join(directory, file_jpg), 'wb') as f:
-    #     x.raw.decode_content=True
-    #     shutil.copyfileobj(x.raw, f)
-
-    # with open(os.path.join(directory, file_jpg), 'rb') as img_file:
-    #     album = img_file.read()
-
-    SONG_META_JSON = get_song_properties(vid_url)
-
-    audio = MP3(os.path.join(directory, file_mp3), ID3=ID3)    
+    audio = MP3(os.path.join(directory, file_mp3), ID3=ID3)
     audio.tags.add(
         APIC(
-            encoding=3, # 3 is for utf-8
-            mime='image/jpeg', # image/jpeg or image/png
-            type=3, # 3 is for the cover image
-            desc=u'Cover',
-            data=SONG_META_JSON['artwork_url_fullres']
+            encoding=3,  # 3 is for utf-8
+            mime="image/jpeg",  # image/jpeg or image/png
+            type=3,  # 3 is for the cover image
+            desc="Cover",
+            data=ITUNES_META_JSON["artwork_url_fullres"],
         )
     )
-    audio["TALB"] = TALB(encoding=3, text=SONG_META_JSON['album_name'])
-    audio["TPE1"] = TPE1(encoding=3, text=SONG_META_JSON['artist_name'])
-    audio["TIT2"] = TIT2(encoding=3, text=SONG_META_JSON['track_name'])
-    audio["TCON"] = TCON(encoding=3, text=SONG_META_JSON['primary_genre_name'])
+    audio["TALB"] = TALB(encoding=3, text=ITUNES_META_JSON["album_name"])
+    audio["TPE1"] = TPE1(encoding=3, text=ITUNES_META_JSON["artist_name"])
+    audio["TIT2"] = TIT2(encoding=3, text=ITUNES_META_JSON["track_name"])
+    audio["TCON"] = TCON(encoding=3, text=ITUNES_META_JSON["primary_genre_name"])
     audio.save()
-    print('hit')
-    os.remove(os.path.join(directory, file_jpg))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- song_property.py
+++ song_property.py
@@ -2,128 +2,47 @@ import requests
 import json
 import itunespy
 from mutagen.mp3 import MP3
-from mutagen.id3 import ID3, APIC, error
-from mutagen.id3 import ID3, TIT2, TALB, TPE1, TPE2, COMM, TCOM, TCON, TDRC
-from PIL import Image
-from io import BytesIO
-import os
+from mutagen.id3 import ID3, APIC, TALB, TPE1, TIT2, TCON, error
 
-song_dir = os.path.join(os.getcwd(), 'Another Summer')
-os.chdir(song_dir)
 
-def get_from_itunes(SONG_NAME):
-    """Try to download the metadata using itunespy."""
-    # Try to get the song data from itunes
-    try:
-        SONG_INFO = itunespy.search_track(SONG_NAME)
-        # Before returning convert all the track_time values to minutes.
-        for song in SONG_INFO:
-            song.track_time = round(song.track_time / 60000, 2)
-        return SONG_INFO
-    except Exception:
-        pass
+def get_from_itunes(song_properties):
+    """Download video metadata using itunespy."""
+    # try:
+    song_itunes = itunespy.search_track(song_properties)
+    # Before returning convert all the track_time values to minutes.
+    for song in song_itunes:
+        song.track_time = round(song.track_time / 60000, 2)
+    return song_itunes
+    # except Exception:
+    #     pass
 
-def get_metadata(url):
-    # KEY https://www.youtube.com/oembed?url={}&format=json
-    URL_KEY = "https://www.youtube.com/oembed?url={}&format=json"
-    content = requests.get(URL_KEY.format(url))
-    json_content = content.json()
 
-    # print(json_content)
+def get_itunes_metadata(vid_url):
+    """Get iTunes metadata to add to mp3 file."""
+    
+    # oEmbed is a format for allowing an embedded representation
+    # of a URL on third party sites.
+    oembed_url = "https://www.youtube.com/oembed?url={}&format=json"
+    vid_content = requests.get(oembed_url.format(vid_url))
+    vid_json = vid_content.json()
 
-    SONG_META = get_from_itunes(json_content['title'])[0]
-    SONG_META_JSON = {
-        'track_name': SONG_META.track_name,
-        'artist_name': SONG_META.artist_name,
-        'album_name': SONG_META.collection_name,
-        'primary_genre_name': SONG_META.primary_genre_name,
-        'artwork_url_fullres': SONG_META.artwork_url_60.replace('60', '600'), # manually replace album artwork to 600x600
+    ITUNES_META = get_from_itunes(vid_json["title"])[0]
+    ITUNES_META_JSON = {
+        "track_name": ITUNES_META.track_name,
+        "album_name": ITUNES_META.collection_name,
+        "artist_name": ITUNES_META.artist_name,
+        "primary_genre_name": ITUNES_META.primary_genre_name,
+        "artwork_url_fullres": ITUNES_META.artwork_url_60.replace(
+            "60", "600"
+        ),  # manually replace album artwork to 600x600
     }
 
-    return SONG_META_JSON
+    def get_album_artwork():
+        """Get album artwork from iTunes album artwork url."""
+        response = requests.get(ITUNES_META_JSON["artwork_url_fullres"])
+        album_img = response.content
+        ITUNES_META_JSON["artwork_url_fullres"] = album_img
 
-def get_song_properties(url):
-    SONG_META_JSON = get_metadata(url)
-    response = requests.get(SONG_META_JSON['artwork_url_fullres'])
-    img = response.content
-    SONG_META_JSON['artwork_url_fullres'] = img
-    return SONG_META_JSON
+        return ITUNES_META_JSON
 
-if __name__ == '__main__':
-    dear_breeze_url = 'https://www.youtube.com/watch?v=mOhaWdKI42E&list=OLAK5uy_m7J5sA6lRSaZ-8Ozc0TnLHA1lck46wHRg&index=3&t=0s'
-    SONG_META_JSON = get_song_properties(dear_breeze_url)
-    file_mp3 = "DEAR BREEZE.mp3"
-
-    audio = MP3(os.path.join(song_dir, file_mp3), ID3=ID3)    
-    audio.tags.add(
-        APIC(
-            encoding=3, # 3 is for utf-8
-            mime='image/jpeg', # image/jpeg or image/png
-            type=3, # 3 is for the cover image
-            desc=u'Cover',
-            data=SONG_META_JSON['artwork_url_fullres']
-        )
-    )
-    audio["TALB"] = TALB(encoding=3, text=SONG_META_JSON['album_name'])
-    audio["TPE1"] = TPE1(encoding=3, text=SONG_META_JSON['artist_name'])
-    audio["TIT2"] = TIT2(encoding=3, text=SONG_META_JSON['track_name'])
-    audio["TCON"] = TCON(encoding=3, text=SONG_META_JSON['primary_genre_name'])
-    audio.save()
-
-# SONG_META = {
-#     'artist_name': 
-# }
-
-# """
-# Python module to search gaana.com using their API.
-# Uses api.gaana.com to get search results.
-# """
-
-# # import requests
-
-# # Define the base url
-# base_url = "http://api.gaana.com/?type=search&subtype=search_song&key={}&token=b2e6d7fbc136547a940516e9b77e5990&format=JSON"
-
-# SONG = []
-
-
-# class GaanaSongs():
-#     """Class to store gaana song tags."""
-
-#     def __init__(self, SONG):
-#         """SONG is supposed to be a dict."""
-#         self.track_name = SONG['track_title']
-#         self.release_date = SONG['release_date']
-#         self.artist_name = SONG['artist'][0]['name']
-#         self.collection_name = SONG['album_title']
-#         self.primary_genre_name = SONG['gener'][0]['name']
-#         self.track_number = '1'
-#         self.artwork_url_100 = SONG['artwork_large']
-#         self.track_time = self._convert_time(SONG['duration'])
-
-#     def _convert_time(self, duration):
-#         in_min = int(duration)
-#         in_time = int(in_min / 60) + (0.01 * (in_min % 60))
-#         return in_time
-
-
-# def searchSong(querry, lim=40):
-#     """Nanan."""
-#     url = base_url.format(querry)
-#     r = requests.get(url)
-#     data = r.json()
-#     data = data['tracks']
-#     SONG_TUPLE = []
-
-#     for i in range(0, len(data)):
-#         song_obj = GaanaSongs(data[i])
-#         SONG_TUPLE.append(song_obj)
-
-#     return SONG_TUPLE
-
-
-# if __name__ == '__main__':
-#     q = "Breathe in the Air"
-#     dat = searchSong(q)
-#     for i in range(0, len(dat)):
-#         print(dat[i].collection_name)
+    return get_album_artwork()
