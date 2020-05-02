db096f928681cb3720c5e3711c2b4ad757610ea9
Ira Horecka <ira89@icloud.com>
2020-05-02 12:15:31 -0700

    Update UI clean repo

--- dev/qt_hacks.py
+++
@@ -1,2 +0,0 @@
-# select entire cell on mouse click
-# self.video_info_input.mousePressEvent = lambda _ : self.video_info_input.selectAll()
--- sandbox/run_thread_test_file.py
+++
@@ -1,7 +0,0 @@
-from thread_test import *
-from pytube import YouTube
-
-x = Multi()
-x.run()
-
-print(YouTube('https://www.youtube.com/watch?v=_PUn7TtnTvw&list=OLAK5uy_lGvTQ9DoMO_m4iKH_GEZv0m1XERbyvU4M&index=2&t=0s'))
